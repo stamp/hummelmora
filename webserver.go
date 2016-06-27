6a92a0d85421238759a25f20c92575f83f4adfda
root <root@hummelmora.(none)>
2016-06-27 13:16:19 +0000

    First working commit

--- webserver.go
+++ webserver.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/olahol/melody"
+	"github.com/stianeikeland/go-rpio"
 )
 
 type webserver struct {
 }
 
 func (self *webserver) Start() {
+	err := rpio.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	heat1 := rpio.Pin(22)
+	heat1.Output()
+
+	heat2 := rpio.Pin(23)
+	heat2.Output()
+
+	lights := rpio.Pin(24)
+	lights.Output()
+
 	r := gin.Default()
 	m := melody.New()
 
@@ -34,7 +51,52 @@ func (self *webserver) Start() {
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
+		type Msg map[string]interface{}
+		data := make(Msg)
+		err := json.Unmarshal(msg, &data)
+		if err != nil {
+			logrus.Error("Failed to decode JSON: ", err)
+			logrus.Debug(string(msg))
+		}
+
+		for key, line := range data {
+			switch key {
+			case "heat1":
+				switch val := line.(type) {
+				case bool:
+					logrus.Info("Heat1=", val)
+					if !val {
+						heat1.High()
+					} else {
+						heat1.Low()
+					}
+				default:
+					logrus.Error("Heat1")
+				}
+			case "heat2":
+				switch val := line.(type) {
+				case bool:
+					logrus.Info("Heat2=", val)
+					if !val {
+						heat2.High()
+					} else {
+						heat2.Low()
+					}
+				default:
+					logrus.Error("Heat2")
+				}
+			case "lights":
+				logrus.Info("Pulse lights")
+				lights.High()
+				<-time.After(time.Millisecond * 200)
+				lights.Low()
+			default:
+				logrus.Error("key", key)
+			}
+		}
+
+		//m.Broadcast(msg)
+
 	})
 
 	go func() {
